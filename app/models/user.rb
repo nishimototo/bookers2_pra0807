f65208169c1b25e2912f2317813150b35651502d
nishimototo <git config --global user.email>
2021-06-19 08:50:33 +0000

    [Finish]

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,10 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   attachment :profile_image
-  
-  has_many :books, dependent
+  validates :name, uniqueness: true, length: {in: 2..20}
+  validates :introduction, length: {maximum: 50}
+
+  has_many :books, dependent: :destroy
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '89532b762bfbf32231dd1749bf9df55b3bbada391a71ec3b8c1b98c0a79117dc79ca40248a1fff97e69dc2b309d6d8cdcd27bb5b6645bd706f770145cd9c0049'
