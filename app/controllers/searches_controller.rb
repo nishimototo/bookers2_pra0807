99c9c04dd8f0e4ac8e88bafa7a300562ff280b98
nishimototo <dkspm2436@gmail.com>
2021-08-09 06:36:24 +0000

    検索機能完了

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,9 +2,9 @@ class SearchesController < ApplicationController
   def search
     @range = params[:range]
     if @range == "User"
-      @users = User.looks(word, search)
+      @users = User.looks(params[:word], params[:search])
     else
-      @books = Book.looks(word, search)
+      @books = Book.looks(params[:word], params[:search])
     end
   end
 end
