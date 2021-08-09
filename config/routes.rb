94b06f4963421cae907437599c5b6339f2a253e3
nishimototo <dkspm2436@gmail.com>
2021-08-09 04:53:19 +0000

    indexとinfoにフォロー機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   devise_for :users
   root "homes#top"
   get "home/about" => "homes#about"
-  resources :users
+
+  resources :users do
+    resource :relationships, only: [:create, :destroy]
+  end
 
   resources :books do
     resources :book_comments, only: [:create, :destroy]
