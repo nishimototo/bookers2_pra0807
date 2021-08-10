371c1cf91ff51c17f670325cdb4b82ce968588e6
nishimototo <dkspm2436@gmail.com>
2021-08-10 01:08:48 +0000

    7b完了

--- app/models/book.rb
+++ app/models/book.rb
@@ -7,6 +7,12 @@ class Book < ApplicationRecord
   has_many :favorited_users, through: :favorites, source: :user
   has_many :book_comments
 
+  scope :created_today, -> { where(created_at: Time.zone.now.all_day) }
+  scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day) }
+  scope :created_this_week, -> { where(created_at: 6.day.ago.beginning_of_day..Time.zone.now.end_of_day) }
+  scope :created_last_week, -> { where(created_at: 13.day.ago.beginning_of_day..1.week.ago.end_of_day) }
+
+
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
   end
